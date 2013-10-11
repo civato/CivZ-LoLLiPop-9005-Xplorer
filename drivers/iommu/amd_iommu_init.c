e6e84662bffbdf99776e27eb28c4a8b9332c90ea
imoseyon <imoseyon@gmail.com>
2013-10-10 23:31:46 -0700

    Linux3.4.5

--- drivers/iommu/amd_iommu_init.c
+++ drivers/iommu/amd_iommu_init.c
@@ -1644,6 +1644,8 @@ static int __init amd_iommu_init(void)
 
 	amd_iommu_init_api();
 
+	x86_platform.iommu_shutdown = disable_iommus;
+
 	if (iommu_pass_through)
 		goto out;
 
@@ -1651,8 +1653,6 @@ static int __init amd_iommu_init(void)
 		printk(KERN_INFO "AMD-Vi: IO/TLB flush on unmap enabled\n");
 	else
 		printk(KERN_INFO "AMD-Vi: Lazy IO/TLB flushing enabled\n");
-
-	x86_platform.iommu_shutdown = disable_iommus;
 
 out:
 	return ret;

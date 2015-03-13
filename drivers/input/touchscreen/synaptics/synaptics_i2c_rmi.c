33628bf00776993786aabd0894746342693304eb
civato <civato@live.be>
2015-03-13 23:55:03 +0100

    HARDLIMIT driver @ yank555.lu

--- drivers/input/touchscreen/synaptics/synaptics_i2c_rmi.c
+++ drivers/input/touchscreen/synaptics/synaptics_i2c_rmi.c
@@ -32,6 +32,10 @@
 #include <linux/regulator/consumer.h>
 #include <linux/qpnp/pin.h>
 
+#ifdef CONFIG_CPUFREQ_HARDLIMIT
+#include <linux/cpufreq_hardlimit.h>
+#endif
+
 #define DRIVER_NAME "synaptics_rmi4_i2c"
 
 #define SYNAPTICS_PM_GPIO_STATE_WAKE	0
@@ -811,8 +815,8 @@ static void synaptics_change_dvfs_lock(struct work_struct *work)
 					__func__);
 		} else {
 			retval = set_freq_limit(DVFS_TOUCH_ID,
-					MIN_TOUCH_LIMIT_SECOND);
-			rmi4_data->dvfs_freq = MIN_TOUCH_LIMIT_SECOND;
+					check_cpufreq_hardlimit(touchboost_lo_freq));
+			rmi4_data->dvfs_freq = check_cpufreq_hardlimit(touchboost_lo_freq);
 		}
 	} else if (rmi4_data->dvfs_boost_mode == DVFS_STAGE_NINTH) {
 		if (rmi4_data->stay_awake) {
@@ -821,8 +825,8 @@ static void synaptics_change_dvfs_lock(struct work_struct *work)
 					__func__);
 		} else {
 			retval = set_freq_limit(DVFS_TOUCH_ID,
-					MIN_TOUCH_LIMIT);
-			rmi4_data->dvfs_freq = MIN_TOUCH_LIMIT;
+					check_cpufreq_hardlimit(touchboost_hi_freq));
+			rmi4_data->dvfs_freq = check_cpufreq_hardlimit(touchboost_hi_freq);
 		}
 	} else if ((rmi4_data->dvfs_boost_mode == DVFS_STAGE_SINGLE) ||
 			(rmi4_data->dvfs_boost_mode == DVFS_STAGE_TRIPLE) ||
@@ -893,18 +897,18 @@ static void synaptics_set_dvfs_lock(struct synaptics_rmi4_data *rmi4_data,
 		if ((!rmi4_data->dvfs_lock_status) || (rmi4_data->dvfs_old_stauts < on)) {
 			cancel_delayed_work(&rmi4_data->work_dvfs_chg);
 
-			if ((rmi4_data->dvfs_freq != MIN_TOUCH_LIMIT) &&
+			if ((rmi4_data->dvfs_freq != check_cpufreq_hardlimit(touchboost_hi_freq)) &&
 					(rmi4_data->dvfs_boost_mode != DVFS_STAGE_NINTH)) {
 				if (rmi4_data->dvfs_boost_mode == DVFS_STAGE_TRIPLE)
 					ret = set_freq_limit(DVFS_TOUCH_ID,
-						MIN_TOUCH_LIMIT_SECOND);
+						check_cpufreq_hardlimit(touchboost_lo_freq));
 				else if (rmi4_data->dvfs_boost_mode == DVFS_STAGE_PENTA)
 					ret = set_freq_limit(DVFS_TOUCH_ID,
-						MIN_TOUCH_LOW_LIMIT);
+						check_cpufreq_hardlimit(touchboost_lo_freq));
 				else
 					ret = set_freq_limit(DVFS_TOUCH_ID,
-						MIN_TOUCH_LIMIT);
-				rmi4_data->dvfs_freq = MIN_TOUCH_LIMIT;
+						check_cpufreq_hardlimit(touchboost_hi_freq));
+				rmi4_data->dvfs_freq = check_cpufreq_hardlimit(touchboost_hi_freq);
 
 				if (ret < 0)
 					dev_err(&rmi4_data->i2c_client->dev,
@@ -1044,7 +1048,7 @@ static void synaptics_tkey_init_dvfs(struct synaptics_rmi4_data *rmi4_data)
 {
 	mutex_init(&rmi4_data->tkey_dvfs_lock);
 	rmi4_data->tkey_dvfs_boost_mode = DVFS_STAGE_DUAL;
-	rmi4_data->tkey_dvfs_freq = MIN_TOUCH_LIMIT_SECOND;
+	rmi4_data->tkey_dvfs_freq = check_cpufreq_hardlimit(touchboost_lo_freq);
 
 	INIT_DELAYED_WORK(&rmi4_data->work_tkey_dvfs_off, synaptics_tkey_set_dvfs_off);
 	INIT_DELAYED_WORK(&rmi4_data->work_tkey_dvfs_chg, synaptics_tkey_change_dvfs_lock);
@@ -1320,6 +1324,7 @@ static int synaptics_rmi4_i2c_read(struct synaptics_rmi4_data *rmi4_data,
 				__func__);
 		retval = -EIO;
 	}
+
 exit:
 	mutex_unlock(&(rmi4_data->rmi4_io_ctrl_mutex));
 
@@ -1384,6 +1389,7 @@ static int synaptics_rmi4_i2c_write(struct synaptics_rmi4_data *rmi4_data,
 				__func__);
 		retval = -EIO;
 	}
+
 exit:
 	mutex_unlock(&(rmi4_data->rmi4_io_ctrl_mutex));
 
